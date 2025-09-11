1c7fc033ff9c62684a3ff4e5c5f2f403361f32bf
CarlosRocamora <carlosrocamora2003@gmail.com>
2025-09-10 23:15:00 -0300

    Adicionando novo teste

---
+++ src/gerenciarUsuarios.js
@@ -0,0 +1,35 @@
+const usuarios = [ 
+    {
+        nome: 'Carlos Henrique',
+        email: 'carloshenrique@gmail.com'
+    },
+    {
+        nome: 'Jorge Fernandez',
+        email: 'jorge_fernandez@gmail.com'
+    },
+    {
+        nome: 'Igor Batista',
+        email: 'igor.batista@gmail.com'
+    },
+    {
+        nome: 'William Guerra',
+        email: 'williamg@gail.com'
+    },
+    {
+        nome: 'Vanessa Freitas',
+        email: 'vanessafreitas@gmail.com'
+    }
+];
+
+function retornarUsuarios() {
+    return usuarios;
+}
+
+function adicionarNovoUsuario(novoUsuario) {
+    usuarios.push(novoUsuario);
+}
+
+module.exports = {
+    retornarUsuarios,
+    adicionarNovoUsuario
+}
